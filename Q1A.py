af17925fba1650f8c60a6056c60b034c710211ff
liwei0908 <nateriver1998@gmail.com>
2020-05-22 21:09:18 +0800

    [feature] add Q1A.py

---
+++ Q1A.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 13:43:45 2020
+
+@author: liwei
+"""
+
+def reverse_singlestr(str1):
+    return str1[::-1] #using python slice    
+
+a1 = input('Input a string to reverse: ')
+print(reverse_singlestr(a1))  
